47d718b20f91551083e44756d8ac9f0ad7f7661d
Vitor Cardoso <vitor.cardoso@hitachivantara.com>
2020-11-18 17:46:33 +0000

    some message

--- tapestry/clean_dfs.sh
+++ tapestry/clean_dfs.sh
@@ -5,3 +5,9 @@ kubectl delete service/data-flow-studio-postgres-cluster -n hitachi-solutions
 kubectl delete service/data-flow-studio-postgres-cluster-config -n hitachi-solutions
 kubectl delete service/data-flow-studio-postgres-cluster-repl -n hitachi-solutions
 kubectl delete pvc pgdata-data-flow-studio-postgres-cluster-0 -n hitachi-solutions
+
+# kubectl delete $(kubectl get clusterrole -o name | grep data-flow)
+# kubectl delete $(kubectl get clusterrolebinding -o name | grep data-flow)
+# kubectl delete clusterrole postgres-pod
+# kubectl delete clusterrole pdi-execution-manager-persistentvolumes
+# kubectl delete clusterrolebinding pdi-execution-manager-persistentvolumes
